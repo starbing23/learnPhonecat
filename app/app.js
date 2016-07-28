21e1e9186f89078bf8358eedd14720828341967d
Bin Wu <starbing23@gmail.com>
2016-07-28 13:55:47 -0400

    step 3

--- app/app.js
+++ app/app.js
@@ -1,7 +1,1 @@
-var phoneCatApp=angular.module("phoneCatApp", []);
-phoneCatApp.controller('PhoneListController', function phoneCatList($scope){
-	$scope.phones=[
-	{name:"motorola", snippet:"cool"},
-	{name:"Dell", snippet:"good"},
-	{name:"Iphone", snippet:"Excellent"}];
-});
+angular.module("phoneCatApp", []);
