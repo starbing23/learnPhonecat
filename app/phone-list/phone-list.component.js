ea8940fd1db51d57e6bf8f9c2818bf96d57b9804
Bin Wu <starbing23@gmail.com>
2016-07-28 14:06:03 -0400

    step-4

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -2,7 +2,7 @@
 
 // Register `phoneList` component, along with its associated controller and template
 
-angular.module('phoneCatApp').
+angular.module('phoneList').
 	component('phoneList',{
 		templateUrl:"phone-list/phone-list.template.html",
 		controller: function PhoneListController(){
